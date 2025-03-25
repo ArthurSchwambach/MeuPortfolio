7d1bccf5867d71dc6a76dc1a12b4309b8ad633c9
Arthur Schwambach <116446407+ArthurSchwambach@users.noreply.github.com>
2025-03-25 14:12:31 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+function openTab(tabName) {
+    // Esconde todos os conteúdos de abas
+    const tabContents = document.getElementsByClassName('tab-content');
+    for (let i = 0; i < tabContents.length; i++) {
+        tabContents[i].classList.remove('active-tab');
+    }
+    
+    // Remove a classe 'active' de todos os botões
+    const tabButtons = document.getElementsByClassName('tab-button');
+    for (let i = 0; i < tabButtons.length; i++) {
+        tabButtons[i].classList.remove('active');
+    }
+    
+    // Mostra a aba atual e marca o botão como ativo
+    document.getElementById(tabName).classList.add('active-tab');
+    event.currentTarget.classList.add('active');
+}
+
 window.addEventListener('load', () => {
     document.body.classList.add('loaded');
 });
